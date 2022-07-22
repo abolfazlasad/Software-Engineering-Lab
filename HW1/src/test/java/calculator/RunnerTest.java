6ab352cbcd7a76c2c231c786b904fe0fdc85c31a
Abolfazl Asad <asad.abolfazl.asad@gmail.com>
2022-07-23 02:54:59 +0430

    [HW1] Correct dependencies

--- HW1/src/test/java/calculator/RunnerTest.java
+++ HW1/src/test/java/calculator/RunnerTest.java
@@ -1,8 +1,8 @@
 package calculator;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features")
 @RunWith(Cucumber.class)
