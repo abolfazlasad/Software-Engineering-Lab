776a4a4c940ef0e1b466eaa4c74d251b073a3291
Abolfazl Asad <asad.abolfazl.asad@gmail.com>
2022-08-04 13:18:43 +0430

    [HW3][Rectangle] add computeArea

--- HW3/src/main/java/Rectangle.java
+++ HW3/src/main/java/Rectangle.java
@@ -1,2 +1,11 @@
 public class Rectangle {
+    private int height;
+    private int width;
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+    public int computeArea() {
+        return height * width;
+    }
 }
--- HW3/src/test/java/RectangleTest.java
+++ HW3/src/test/java/RectangleTest.java
@@ -1,8 +1,15 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class RectangleTest {
     @Test
-    public void checkRectangleCreation() {
-        Rectangle rectangle = new Rectangle();
+    public void checkComputeArea() {
+        // Arrange
+        Rectangle rectangle = new Rectangle(3, 2);
+        // Act
+        int area = rectangle.computeArea();
+        // Assert
+        assertEquals(6, area);
     }
 }
