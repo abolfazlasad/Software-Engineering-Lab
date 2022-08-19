d96f5fbd9eca4c7184be7c0d5e195f45df962f59
Abolfazl Asad <asad.abolfazl.asad@gmail.com>
2022-08-19 16:37:22 +0430

    [HW5][Coverage] add test for parser

--- HW5/Coverage/json-simple/src/test/java/org/json/simple/TestJson.java
+++ HW5/Coverage/json-simple/src/test/java/org/json/simple/TestJson.java
@@ -4,13 +4,17 @@
  */
 package org.json.simple;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.json.simple.parser.ParseException;
 
 public class TestJson {
 
@@ -89,5 +93,47 @@ public class TestJson {
 		assertEquals(charsStr,
 				String.format("[\"%c\",\"%c\",\"%c\"]", 101, 102, 103));
 		assertEquals(floatsStr, "[101.0,102.0,103.0]");
+	}
+
+	@Test
+	public void testParseFloatAndInt() {
+		// Arrange
+		String s = "{\"float\":[0.0],\"int\":[0]}";
+		JSONObject obj= (JSONObject) JSONValue.parse(s);
+		// Act
+		JSONArray intArray = (JSONArray) obj.get("int");
+		JSONArray floatArray = (JSONArray) obj.get("float");
+		// Assert
+		assertTrue(intArray.get(0) instanceof Long);
+		assertTrue(floatArray.get(0) instanceof Double);
+	}
+
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	@Test
+	public void TestErrorInParseCloseBrace() throws ParseException {
+		// Arrange, Assert
+		String s = "{\"key\":\"value\"";
+		thrown.expect(ParseException.class);
+		thrown.expectMessage(
+				String.format("Unexpected token END OF FILE at position %d.", s.length()));
+		// Act
+		JSONValue.parseWithException(s);
+	}
+
+	@Test()
+	public void testMissingColon() {
+		// Arrange
+		String s = "{\"key\";\"value\"}";
+		try {
+			// Act
+			JSONValue.parseWithException(s);
+		} catch (ParseException e) {
+			// Assert
+			assertEquals(e.getPosition(), s.indexOf(';'));
+			assertEquals(e.getUnexpectedObject().toString(), ";");
+			assertEquals(e.getErrorType(), ParseException.ERROR_UNEXPECTED_CHAR);
+		}
 	}
 }
